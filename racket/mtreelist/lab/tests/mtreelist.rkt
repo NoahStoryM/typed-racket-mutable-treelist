242f10232c6e251547a87d232c8448c45eee56d8
NoahStoryM <noahstorym@gmail.com>
2024-12-14 13:19:14 +0800

    Update

--- racket/mtreelist/lab/tests/mtreelist.rkt
+++ racket/mtreelist/lab/tests/mtreelist.rkt
@@ -146,14 +146,14 @@
   (mtreelist-set! mtl0 0 'a)
   (check-equal? (mtreelist->list mtl0) '(a 1 2 3 4))
 
-  (define mtl1 (for/mtreelist : (Mutable-TreeListof Natural Integer) ([i 5]) i))
+  (define mtl1 (for/mtreelist : (MTreeListof Natural Integer) ([i 5]) i))
   (mtreelist-set! mtl1 0 9)
   (check-equal? (mtreelist->list mtl1) '(9 1 2 3 4))
 
-  (define mtl2 (for/mtreelist ([i 5]) : (Mutable-TreeListof Natural Integer) i))
+  (define mtl2 (for/mtreelist ([i 5]) : (MTreeListof Natural Integer) i))
   (mtreelist-set! mtl2 0 9)
   (check-equal? (mtreelist->list mtl2) '(9 1 2 3 4))
 
-  (define mtl3 (for/mtreelist : (Mutable-TreeListof Natural Integer) ([i 5]) : (Mutable-TreeListof Natural Natural) i))
+  (define mtl3 (for/mtreelist : (MTreeListof Natural Integer) ([i 5]) : (MTreeListof Natural Natural) i))
   (mtreelist-set! mtl3 0 9)
   (check-equal? (mtreelist->list mtl3) '(9 1 2 3 4)))
